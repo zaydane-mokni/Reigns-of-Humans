00ef73c2f44e20f34904b355c367d8972f81e03a
Mjulien-000 <julien.meunierrr@gmail.com>
2019-10-08 09:57:19 +0200

    accueil rules php

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title> Accueil </title>
+        <?php  include('header.php'); ?>
+    </head>
+
+<body>
+    <header>
+    
+    </header>
+
+
+
+
+</body>
---
+++ rules.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title> Rules </title>
+        <?php  include('header.php'); ?>
+    </head>
+
+<body>
+    <header>
+    
+    </header>
+
+    <div class="container-fluid text-center">    
+        <div class="row content">
+            <div class="col-sm-2 sidenav">
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+                <p><a href="#">Link</a></p>
+            </div>
+            <div class="col-sm-8 text-left"> 
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://via.placeholder.com/800x400.png?text=Visit+WhoIsHostingThis.com+Buyers+Guide" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://via.placeholder.com/800x400.png?text=Visit" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://via.placeholder.com/800x400.png?text=Visit+WhoIsHostingThis.com" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div> 
+            </div>
+            <div class="col-sm-2 sidenav">
+                <div class="well">
+                    <p>ADS</p>
+                </div>
+                <div class="well">
+                    <p>ADS</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
